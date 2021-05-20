31acaa8a01043d40aec62a6342d51cdcfb3724d6
Joshua Purcell <hello@vuld.in>
2021-05-20 13:49:10 -0400

    add getRegion, refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,72 +1,9 @@
-import src from './seededRandomColor.js'
-import getVector from './getVector.js'
+import {
+    getRegionById,
+    getRegionByVector,
+} from './getRegion.js'
 
-let regionCache = []
-const vectorAccessor = region => region.vector
-
-function getRegionByVector(vector) {
-  let region = regionCache.find(({ vector: {x, y, z}}) =>
-    x === vector.x
-    && y === vector.y
-    && z === vector.z
-  )
-  if(!region) {
-    let regionNotFound = true
-    for(let nextId = regionCache.length; regionNotFound; nextId++) {
-      const newRegion = getRegionById(nextId)
-      const newRegionVector = newRegion.vector
-      if(
-        newRegionVector.x === vector.x
-        && newRegionVector.y === vector.y
-        && newRegionVector.z === vector.z
-      ) {
-        region = newRegion
-        regionNotFound = false
-      }
-    }
-  }
-  return region
+export {
+    getRegionById,
+    getRegionByVector,
 }
-
-function getRegionById(num) {
-  let region
-  if(regionCache.length > num) { // found
-    region = regionCache[num]
-  } else { // not found
-    let regionNotFound = true
-    for(let nextId = regionCache.length; regionNotFound; nextId++) {
-      regionCache.push({
-        id: nextId,
-        name: `region-${nextId}`,
-        color: src(`${nextId}`),
-        vector: getVector(nextId, regionCache, vectorAccessor),
-      })
-      if(num === nextId) {
-        region = regionCache[num]
-        regionNotFound = false
-      }
-    }
-  }
-  return region
-}
-
-// TODO const objects = []
-
-//[...Array(200).keys()].map(getRegionById)
-/*
-const region0 = getRegionById(0)
-const region1 = getRegionById(1)
-const region199 = getRegionById(199)
-*/
-//regions.forEach(({ vector }) => console.log(vector))
-
-const region0 = getRegionByVector({x: 0, y: 0, z: 0})
-const region1 = getRegionByVector({x: -1, y: -1, z: -1})
-const region199 = getRegionByVector({x: -1, y: -3, z: -2})
-
-console.log(region0)
-console.log(region1)
-console.log(region199)
-
-console.log('cache length', regionCache.length)
-console.log(regionCache[2])
